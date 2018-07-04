bc71f480983081e6a581be745ce7611e260e7dd3
Tianhong Chu <cthesky@yeah.net>
2018-07-04 22:27:33 +0800

    :hammer: remove tuple method and add comment for Auth

--- namecom/auth.py
+++ namecom/auth.py
@@ -2,11 +2,21 @@ __all__ = ['Auth']
 
 
 class Auth:
+    """
+    The class for http basic authentication.
+    Used by api class to do authenticate request.
+    Could be found at token manage page: https://www.name.com/account/settings/api
+    """
 
     def __init__(self, username, token):
+        """
+        Parameters
+        ----------
+        username : string
+            username from token manage page
+        token : string
+            token value from token manage page
+        """
         self.username = username
         self.token = token
 
-    @property
-    def auth_tuple(self):
-        return self.username, self.token
