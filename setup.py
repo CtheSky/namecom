224e0ef51a4731f8c4909b83342ab569ed455568
Tianhong Chu <cthesky@yeah.net>
2018-08-15 22:19:33 +0800

    :card_index: move setup metadata to setup.cfg & MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
-import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
-this_dir = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_dir, 'requirements.txt')) as f:
-    requirements = [line.strip() for line in f.readlines()]
-
-with open(os.path.join(this_dir, 'README.rst')) as f:
-    long_description = f.read()
-
-setup(
-    name='pynamecom',
-    version='0.1.2',
-    description='Python Library for Name.com v4 API',
-    long_description=long_description,
-    author='Tianhong Chu',
-    author_email='cthesky13@gmail.com',
-    url='https://github.com/CtheSky/namecom',
-    packages=find_packages(exclude=("tests", "tests.*")),
-    license='MIT',
-    platforms=['any'],
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
-    install_requires=requirements
-)
+setup()
